378f9554bb1f497cd2008c3ee632d593d3c7f8c1
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-12-24 14:12:18 +0700

    Updates

--- src/backend/src/utils/ApiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-  constructor(message, statusCode, name = null) {
-    super(message);
-    this.name = name || this.constructor.name;
-    this.status = statusCode;
-  }
-}
-
-module.exports = ApiError;
